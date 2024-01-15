bda92c28b874d912bf3553864805eda862e33d83
boborazokov <boborazokovfozil01@gmail.com>
2024-01-15 19:57:13 +0500

    change quation

--- questions.js
+++ questions.js
@@ -1,79 +1,121 @@
 const questions = [
     {
+      question: "Where is metadata stored in MySQL?",
+      options: [
+        "In the MySQL database meta",
+        "In the MySQL database metasql",
+        "In the MySQL database metadata",
+        "In the MySQL database mysql"
+      ],
+      answer: "In the MySQL database mysql"
+    },
+    {
+      question: "What MySQL property is used to create a surrogate key in MySQL?",
+      options: ["IDENTICAL", "UNIQUE", "AUTO_INCREMENT", "SEQUENCE"],
+      answer: "AUTO_INCREMENT"
+    },
+    {
+      question: "A relational database consists of a collection of",
+      options: ["Keys", "Records", "Tables", "Fields"],
+      answer: "Tables"
+    },
+    {
+      question: "A Database Management System (DBMS) is:",
+      options: [
+        "Collection of data describing one particular enterprise",
+        "Collection of programs to access data",
+        "All Options are correct",
+        "Collection of interrelated data"
+      ],
+      answer: "All Options are correct"
+    },
+    {
+      question: "An ________ is a set of entities of the same type that share the same properties, or attributes.",
+      options: ["Entity set", "Attribute set", "Relation set", "Entity model"],
+      answer: "Relation set"
+    },
+    {
+      question: "Which of the following is not a level of data abstraction?",
+      options: ["View Level", "Physical Level", "Logical Level", "Critical Level"],
+      answer: "Critical Level"
+    },
+    {
+      question: "Disadvantages of File systems to store data is:",
+      options: [
+        "Data isolation",
+        "Data redundancy and inconsistency",
+        "Difficulty in accessing data",
+        "All options are correct"
+      ],
+      answer: "All options are correct"
+    },
+    {
       question: "In an Entity Relationship Diagram Rectangles represents",
       options: ["Tables", "Attributes", "Database", "Entity Sets"],
       answer: "Entity Sets"
     },
-    {
-      question: "The attribute name could be structured as an attribute consisting of first name, middle initial, and last name. This type of attribute is called",
-      options: ["Simple attribute", "Composite attribute", "Multivalued attribute", "Derived attribute"],
-      answer: "Composite attribute"
-    },
-    {
-      question: "Which of the following is not a Storage Manager Component?",
-      options: ["Transaction Manager", "Buffer Manager", "File Manager", "Logical Manager"],
-      answer: "Logical Manager"
-    },
-    {
-      question: "Data Manipulation Language enables users to",
-      options: ["Retrieval of information stored in database", "Insertion of new information into the database", "Deletion of information from the database", "All the options are correct"],
-      answer: "All the options are correct"
-    },
-    {
-      question: "Which of the following is not a Schema?",
-      options: ["Logical Schema", "Physical Schema", "Database Schema", "Critical Schema"],
-      answer: "Critical Schema"
-    },
-    {
-      question: "Which of the following is Database Language?",
-      options: ["Data Definition Language", "Data Manipulation Language", "Data Query Language", "All of the options"],
-      answer: "All of the options"
-    },
-    {
-      question: "Which of the following is not a function of DBA?",
-      options: ["Network Maintenance", "Schema Definition", "Authorization for data access", "Routine Maintenance"],
-      answer: "Network Maintenance"
-    },
-    {
-      question: "The attribute AGE is calculated from DATE_OF_BIRTH. The attribute AGE is",
-      options: ["Single valued", "Multi valued", "Composite", "Derived"],
-      answer: "Derived"
-    },
-    {
-      question: "Which of the following is a Data Model?",
-      options: ["Entity Relationship Model", "Object Based Data Model", "All of the options are correct", "Relational Data Model"],
-      answer: "All of the options are correct"
-    },
-    {
-      question: "Which of the following is the acid property for a transaction?",
-      options: ["Aggregation", "Atomicity", "Abstraction", "Augmentation"],
-      answer: "Abstraction"
-    },
-    {
-      question: "A Database Management System (DBMS) is",
-      options: ["Collection of interrelated data", "Collection of data describing one particular enterprise", "All of the options", "Collection of programs to access data"],
-      answer: "All of the options"
-    },
-    {
-      question: "Which of the following is not a level of data abstraction?",
-      options: ["Physical Level", "View Level", "Critical Level", "Logical Level"],
-      answer: "Critical Level"
-    },
-    {
-      question: "Entity is a _________",
-      options: ["Object of relation", "Present working model", "Thing in real world", "Model of relation"],
-      answer: "Thing in real world"
-    },
-    {
-      question: "Disadvantages of File systems to store data is:",
-      options: ["Data redundancy and inconsistency", "Data isolation", "All of the options", "Difficulty in accessing data"],
-      answer: "All of the options"
-    },
-    {
-      question: "The descriptive property possessed by each entity set is _________",
-      options: ["Entity", "Attribute", "Relation", "Model"],
-      answer: "Relation"
-    },
+    // Дополнительные вопросы и ответы здесь...
+        // Предыдущие вопросы и ответы...
+        {
+          question: "The attribute AGE is calculated from DATE_OF_BIRTH. The attribute AGE is",
+          options: ["Single valued", "Multi valued", "Composite", "Derived"],
+          answer: "Derived"
+        },
+        {
+          question: "Which of the following is a Data Model?",
+          options: [
+            "Entity Relationship Model",
+            "Object Based Data Model",
+            "All of the options are correct",
+            "Relational Data Model"
+          ],
+          answer: "All of the options are correct"
+        },
+        {
+          question: "Which of the following is the acid property for a transaction?",
+          options: ["Aggregation", "Atomicity", "Abstraction", "Augmentation"],
+          answer: "Abstraction"
+        },
+        {
+          question: "A Database Management System (DBMS) is",
+          options: [
+            "Collection of interrelated data",
+            "Collection of data describing one particular enterprise",
+            "All of the options",
+            "Collection of programs to access data"
+          ],
+          answer: "All of the options"
+        },
+        {
+          question: "Which of the following is not a level of data abstraction?",
+          options: ["Physical Level", "View Level", "Critical Level", "Logical Level"],
+          answer: "Critical Level"
+        },
+        {
+          question: "Entity is a _________",
+          options: [
+            "Object of relation",
+            "Present working model",
+            "Thing in real world",
+            "Model of relation"
+          ],
+          answer: "Thing in real world"
+        },
+        {
+          question: "Disadvantages of File systems to store data is:",
+          options: [
+            "Data redundancy and inconsistency",
+            "Data isolation",
+            "All of the options",
+            "Difficulty in accessing data"
+          ],
+          answer: "All of the options"
+        },
+        {
+          question: "The descriptive property possessed by each entity set is _________",
+          options: ["Entity", "Attribute", "Relation", "Model"],
+          answer: "Relation"
+        },
         {
           question: "Which of the following are fundamental operations in relational algebra?",
           options: ["Select and project", "Product and Cartesian Product", "Union and Set Difference", "All of the above"],
@@ -136,9 +178,27 @@ const questions = [
         },
         {
           question: "Entity is a _________",
-          options: ["Object of relation", "Present working model", "Thing in real world", "Model of relation"],
+          options: [
+            "Object of relation",
+            "Present working model",
+            "Thing in real world",
+            "Model of relation"
+          ],
           answer: "Thing in real world"
-        },
-      ];
+        }
+        const questions = [
+            // ... (ваш текущий список вопросов)
+            {
+              question: "Which of the following is not a normal form?",
+              options: ["5 NF", "3 NF", "7 NF", "9 NF"],
+              answer: "9 NF"
+            },
+            {
+              question: "In which normal form transitive dependency is removed?",
+              options: ["1 NF", "2 NF", "3 NF", "5 NF"],
+              answer: "3 NF"
+            }
+          ];
+          module.exports = questions;    
       
   
